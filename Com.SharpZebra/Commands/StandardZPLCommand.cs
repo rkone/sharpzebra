6b9b35e48b6504faaeec226a1dd0ad2b51c5d7be
Rob Morriss <rob@morrisshome.com>
2021-01-15 13:29:48 -0500

    Updated to allow tilde characters to print

--- Com.SharpZebra/Commands/StandardZPLCommand.cs
+++ Com.SharpZebra/Commands/StandardZPLCommand.cs
@@ -6,6 +6,11 @@ namespace SharpZebra.Commands
 {
     public partial class ZPLCommands
     {
+        /// <summary>
+        /// Initializes printer print speed, tear off, alignment, width and darkness
+        /// </summary>
+        /// <param name="settings">The variable containing all required settings</param>
+        /// <returns>Array of bytes containing ZPLII data to be sent to the Zebra printer.</returns>
         public static byte[] ClearPrinter(Printing.PrinterSettings settings)
         {
             //^MMT: Tear off Mode.  ^PRp,s,b: print speed (print, slew, backfeed) (2,4,5,6,8,9,10,11,12).  
@@ -17,25 +22,41 @@ namespace SharpZebra.Commands
                 settings.AlignTearOff, settings.AlignLeft, settings.AlignTop, settings.Darkness, settings.Width + settings.AlignLeft));
         }
 
+        /// <summary>
+        /// Instruct the Zebra printer to print labels
+        /// </summary>
+        /// <param name="copies">The number of identical copies of the label to print</param>
+        /// <returns>>Array of bytes containing ZPLII data to be sent to the Zebra printer.</returns>
         public static byte[] PrintBuffer(int copies  = 1)
         {
             return Encoding.GetEncoding(850).GetBytes(copies > 1 ? $"^PQ{copies}^XZ" : "^XZ");
         }
 
+        /// <summary>
+        /// Instruct the Zebra printer to print a barcode.  Currently only 3of9, Code128, UPC_A and EAN13 are supported.
+        /// </summary>
+        /// <param name="left">Distance in dots from the left of the label</param>
+        /// <param name="top">Distance in dots to the top of the label</param>
+        /// <param name="height">Height in dots of the barcode</param>
+        /// <param name="rotation">Rotate field.</param>
+        /// <param name="barcode">Type and parameters of the barcode to print.</param>
+        /// <param name="readable">Enable text to be printed at the bottom of the barcode.</param>
+        /// <param name="barcodeData">Text to encode in the barcode</param>
+        /// <returns>>Array of bytes containing ZPLII data to be sent to the Zebra printer.</returns>
         public static byte[] BarCodeWrite(int left, int top, int height, ElementDrawRotation rotation, Barcode barcode, bool readable, string barcodeData)
         {
             var encodedReadable = readable ? "Y" : "N";
             switch (barcode.Type)
             {
                 case BarcodeType.CODE39_STD_EXT:
-                    return Encoding.GetEncoding(850).GetBytes(string.Format("^FO{0},{1}^BY{2}^B3{3},,{4},{5}^FD{6}^FS", left, top,
-                        barcode.BarWidthNarrow, (char)rotation, height, encodedReadable, barcodeData));
+                    return Encoding.GetEncoding(850).GetBytes(
+                        $"^FO{left},{top}^BY{barcode.BarWidthNarrow}^B3{(char) rotation},,{height},{encodedReadable}^FD{barcodeData}^FS");
                 case BarcodeType.CODE128_AUTO:
-                    return Encoding.GetEncoding(850).GetBytes(string.Format("^FO{0},{1}^BY{2}^BC{3},{4},{5}^FD{6}^FS", left, top,
-                        barcode.BarWidthNarrow, (char)rotation, height, encodedReadable, barcodeData));
+                    return Encoding.GetEncoding(850).GetBytes(
+                        $"^FO{left},{top}^BY{barcode.BarWidthNarrow}^BC{(char) rotation},{height},{encodedReadable}^FD{barcodeData}^FS");
                 case BarcodeType.UPC_A:
-                    return Encoding.GetEncoding(850).GetBytes(string.Format("^FO{0},{1}^BY{2}^BU{3},{4},{5}^FD{6}^FS", left, top,
-                        barcode.BarWidthNarrow, (char)rotation, height, encodedReadable, barcodeData));
+                    return Encoding.GetEncoding(850).GetBytes(
+                        $"^FO{left},{top}^BY{barcode.BarWidthNarrow}^BU{(char) rotation},{height},{encodedReadable}^FD{barcodeData}^FS");
                 case BarcodeType.EAN13:
                     return Encoding.GetEncoding(850).GetBytes(string.Format("^FO{0},{1}^BY{2}^BE{3},{4},{5}^FD{6}^FS", left, top,
                         barcode.BarWidthNarrow, (char)rotation, height, encodedReadable, barcodeData));
@@ -84,7 +105,7 @@ namespace SharpZebra.Commands
         [Obsolete("Use ZPLFont instead of ZebraFont.")]
         public static byte[] TextWrite(int left, int top, ElementDrawRotation rotation, ZebraFont font, int height, int width = 0, string text = "", int codepage = 850)
         {
-            return Encoding.GetEncoding(codepage).GetBytes($"^FO{left},{top}^A{(char)font}{(char)rotation},{height},{width}^FD{text}^FS");
+            return Encoding.GetEncoding(codepage).GetBytes($"^FO{left},{top}^A{(char)font}{(char)rotation},{height},{width}{FixTilde(text)}FH");
         }
 
         /// <summary>
@@ -104,7 +125,7 @@ namespace SharpZebra.Commands
         /// <returns>Array of bytes containing ZPLII data to be sent to the Zebra printer.</returns>
         public static byte[] TextWrite(int left, int top, ElementDrawRotation rotation, ZPLFont font, int height, int width = 0, string text = "", int codepage = 850)
         {
-            return Encoding.GetEncoding(codepage).GetBytes($"^FO{left},{top}^A{(char)font}{(char)rotation},{height},{width}^FD{text}^FS");
+            return Encoding.GetEncoding(codepage).GetBytes($"^FO{left},{top}^A{(char)font}{(char)rotation},{height},{width}{FixTilde(text)}");
         }
 
         /// <summary>
@@ -124,7 +145,7 @@ namespace SharpZebra.Commands
         public static byte[] TextWrite(int left, int top, ElementDrawRotation rotation, string fontName, char storageArea, int height, string text, int codepage = 850)
         {
             var rotationValue = (char) rotation;
-            return Encoding.GetEncoding(codepage).GetBytes(string.Format("^A@{0},{1},{1},{2}:{3}^FO{4},{5}^FD{6}^FS", rotationValue, height, storageArea, fontName, left, top, text));
+            return Encoding.GetEncoding(codepage).GetBytes(string.Format("^A@{0},{1},{1},{2}:{3}^FO{4},{5}{6}", rotationValue, height, storageArea, fontName, left, top, FixTilde(text)));
         }
 
         /// <summary>
@@ -142,14 +163,32 @@ namespace SharpZebra.Commands
         public static byte[] TextWrite(int left, int top, ElementDrawRotation rotation, int height, string text, int codepage = 850)
         {
             //uses last specified font
-            return Encoding.GetEncoding(codepage).GetBytes($"^A@{(char) rotation},{height}^FO{left},{top}^FD{text}^FS");
+            return Encoding.GetEncoding(codepage).GetBytes($"^A@{(char) rotation},{height}^FO{left},{top}{FixTilde(text)}");
         }
 
+        /// <summary>
+        /// Encases a Textwrite into a alignable box. Top left corner is determined in the TextWrite command.
+        /// </summary>
+        /// <param name="width">Width of the box to align the text inside</param>
+        /// <param name="alignment">Left, right, centered, justified</param>
+        /// <param name="textCommand">Results of a TextWrite command</param>
+        /// <returns>Array of bytes containing ZPLII data to be sent to the Zebra printer.</returns>
         public static byte[] TextAlign(int width, Alignment alignment, byte[] textCommand)
         {
             return TextAlign(width, alignment, 1, 0, 0, textCommand);
         }
 
+        /// <summary>
+        /// Encases a Textwrite into a alignable box with custom line heights and a maximum height. Top left corner is determined in the TextWrite command.
+        /// </summary>
+        /// <param name="width">Width of the alignment box</param>
+        /// <param name="alignment">Left, right, centered, justified</param>
+        /// <param name="maxLines">maximum lines to allow the text wrap before cutting it off</param>
+        /// <param name="lineSpacing">dots between each line</param>
+        /// <param name="indentSize">dots to indent after the first line</param>
+        /// <param name="textCommand">Results of a TextWrite command</param>
+        /// <param name="codepage">The text encoding page the printer is set to use</param>
+        /// <returns>Array of bytes containing ZPLII data to be sent to the Zebra printer.</returns>
         public static byte[] TextAlign(int width, Alignment alignment, int maxLines, int lineSpacing, int indentSize, byte[] textCommand, int codepage = 850)
         {
             //limits from ZPL Manual:
@@ -187,9 +226,19 @@ namespace SharpZebra.Commands
 
         public static byte[] BoxWrite(int left, int top, int lineThickness, int width, int height, int rounding)
         {
-            return Encoding.GetEncoding(850).GetBytes(string.Format("^FO{0},{1}^GB{2},{3},{4},{5},{6}^FS", left, top,
-                Math.Max(width, lineThickness), Math.Max(height, lineThickness), lineThickness, "", rounding));
+            return Encoding.GetEncoding(850).GetBytes(
+                $"^FO{left},{top}^GB{Math.Max(width, lineThickness)},{Math.Max(height, lineThickness)},{lineThickness},,{rounding}^FS");
         }
+
+        private static string FixTilde(string text)
+        {
+            if (!text.Contains("~"))
+                return $"^FD{text}^FS";
+            if (text.Contains("_"))
+                throw new ApplicationException("Tilde character is not supported with underscore in same command");
+            return $"^FH^FD{text.Replace("~", "_7e")}^FS";
+        }
+        
         /*
         public static string FormDelete(string formName)
         {
